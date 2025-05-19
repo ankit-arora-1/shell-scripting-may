68aa8a2d6ebf867fe81f5ee8e9df2c35c385c56a
ankit-arora-1 <135776379+ankit-arora-1@users.noreply.github.com>
2025-05-20 00:05:30 +0530

    Create script2.sh

---
+++ script2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Display current date and time
+# Show the current working directory
+# List all files in long format
+# Print a message "End of Message"
+
+echo "Printing the date"
+date
+
+echo "Current working dir"
+pwd
+
+echo "These are all the files"
+ls -al
+
+echo "End of message"
